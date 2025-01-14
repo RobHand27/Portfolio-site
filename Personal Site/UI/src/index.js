5284616d1568922a2e91684f1c26a3d9903c1bba
roberthand <rhand7@illinois.edu>
2025-01-14 01:08:03 -0600

    test

--- Personal Site/UI/src/index.js
+++ Personal Site/UI/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App'; // where pages are routed. TODO later
-import { BrowserRouter } from 'react-router-dom';
+// import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
